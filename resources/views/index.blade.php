7fabd0b8458a7974af0b1369d61aff82245e5c9e
asmi046 <asmi046@gmail.com>
2025-04-01 19:15:36 +0300

    Корректировка отзывов

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -77,7 +77,7 @@
 
     <section class="review_section">
         <div class="container">
-            <h2>Клиенты пишут о нашей компании:</h2>
+            <h2 class="h2_in_page">Отзывы <br><span class="gold">довольных</span> клиентов</h2>
             <div id="review_app" class="review_app">
                 <review></review>
             </div>
