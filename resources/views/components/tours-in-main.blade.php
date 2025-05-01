5bd7b56c89eb9cfadec5af8cb2a150cab5a21713
asmi046 <asmi046@gmail.com>
2025-05-01 04:34:28 +0300

    +1

--- resources/views/components/tours-in-main.blade.php
+++ resources/views/components/tours-in-main.blade.php
@@ -1,5 +1,7 @@
-<div class="all_tour_wrapper">
+<div class="all_tour_wrapper greed">
     @foreach ($all_tours as $item)
-        <x-tour-card :item="$item"></x-tour-card>
+        <div class="g_cell">
+            <x-tour-card :item="$item"></x-tour-card>
+        </div>
     @endforeach
 </div>
